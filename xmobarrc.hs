574f3f11bdbf183999606270a7c863b94df46925
k4l1brx <tr4nquoc@gmail.com>
2020-12-06 22:51:38 +0700

    README new screenshot

--- xmobarrc.hs
+++ xmobarrc.hs
@@ -49,27 +49,6 @@ Config { font    = "xft:Inconsolata Nerd Font:size=9:antialias=true:hinting=true
                       
                     ]
 
-                      -- Cpu used in percent
-                    -- , Run Cpu ["-t", "\xf108 cpu: (<total>%)","-H","50","--high","red"] 20
-
-                      -- Ram used number and percent
-                    -- , Run Memory ["-t", "\xf233 mem: <used>M (<usedratio>%)"] 20
-
-                      -- Disk space free
-                    -- , Run DiskU [("/home", "\xf0c7 ssd: <free> free")] [] 60
-
-                      -- Runs custom script to check for zypper updates.
-                      -- Needs changing based on the package manager in use.
-                    -- , Run Com "/bin/bash" ["-c", "~/.scripts/pacupdate"] "check"  30
-
-                      -- Cpu temp in centigrade using a script
-                    -- , Run Com "/bin/bash" ["-c", "~/.scripts/ctemp"] "ctemp"  2
-
-                      -- Nvidia gpu temp in centigrade using nvidia-smi
-                    -- , Run Com "nvidia-smi" ["--query-gpu=temperature.gpu", "--format=csv,noheader,nounits"] "gpu" 2
-
-                      -- Runs a standard shell command 'uname -r' to get kernel version
-                    -- , Run Com "uname" ["-r"] "ker" 0
                     , Run Kbd            [ ("de" , "<fc=#FABD2F>\xf40b  DE</fc>")
                              , ("us"         , "<fc=#FABD2F>\xf40b  US</fc>")
                              ]
@@ -80,6 +59,5 @@ Config { font    = "xft:Inconsolata Nerd Font:size=9:antialias=true:hinting=true
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       --- , template = " <icon=haskell_20.xpm/> <fc=#666666>|</fc> %UnsafeStdinReader% }{ <fc=#b3afc2> %ker% </fc><fc=#666666>| </fc><fc=#f57900>gpu: %gpu%°C cpu: %ctemp%°C </fc><fc=#666666>| </fc><fc=#ff5252>%cpu% </fc><fc=#666666>| </fc><fc=#ffc135>%memory% </fc><fc=#666666>| </fc><fc=#16a085>%disku% </fc><fc=#666666>| </fc><fc=#ff5c79> updates: %check%</fc><fc=#666666>| </fc><fc=#42a5f5>%alsa:pulse:Master%</fc><fc=#666666>| </fc>"
            , template = "<action=`~/.scripts/rofi_app_launcher.sh`><fc=#FABD2F><icon=haskell_20.xpm/></fc></action> %UnsafeStdinReader%}{<action=`~/.scripts/rofi_wifi_menu.sh`>%dynnetwork% %wi%</action><fc=#665c24>|</fc><action=`xfce4-power-manager-settings`> %battery% </action><fc=#665c24>|</fc><action=`~/.scripts/Toggle_Keymap.sh`>%kbd%</action><fc=#665c24>|</fc> %alsa:pulse:Master%<fc=#665c24>|</fc>%date% %trayerpad%"
        }
